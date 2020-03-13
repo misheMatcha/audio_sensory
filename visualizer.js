fd1c11a98d612b3fe16130148b868a66fbb0d388
misheMatcha <mishe.vong@gmail.com>
2020-03-13 09:24:16 -0700

    setup audio buffer

--- visualizer.js
+++ visualizer.js
@@ -1,7 +1,13 @@
 window.AudioContext = window.AudioContext || window.webkitAudioContext;
-const audioCtx = new AudioContext();
+const audioContext = new AudioContext();
 let currentBuffer = null;
 
-const url = 'https://audio-viz.s3-us-west-1.amazonaws.com/pornograffiti_the-day.mp3';
+// allows for cors for the s3 link
+const url = "https://cors-anywhere.herokuapp.com/" + "https://audio-viz.s3-us-west-1.amazonaws.com/pornograffiti_the-day.mp3";
 
-// #e8b923
+const visualizeAudio = url => {
+  fetch(url)
+    .then(res => res.arrayBuffer())
+    .then(arrayBuffer => audioContext.decodeAudioData(arrayBuffer))
+    .then(audioBuffer => visualizeAudio(audioBuffer))
+};
